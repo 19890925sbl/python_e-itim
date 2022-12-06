f8f0b37064eca18724a1cd4c1c6076398041b7a1
19890925sbl <mercansibel1989@gmail.com>
2022-12-06 20:05:44 +0300

    yorum_satırları

--- type-conversion.py
+++ type-conversion.py
@@ -6,13 +6,15 @@ print(type(y))
 toplam=int(x)+int(y)
 print(toplam)'''
 
-x=5
-y=2.5
-name="Çınar"
-isOnline=True
+# x=5
+# y=2.5
+# name="Çınar"
+# isOnline=True
 
-print(type(x))
-print(type(y))
-print(type(name))
-print(type(isOnline))
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+
 
