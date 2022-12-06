f69b2546e4201c8145ad9455af72fe11466a0e29
19890925sbl <mercansibel1989@gmail.com>
2022-12-06 22:03:01 +0300

    index-2

--- strings.py
+++ strings.py
@@ -3,9 +3,15 @@ surname='Mercan'
 age=39
 
 greeting='My name is'+name+''+surname+'and n\I am'+str(age)+'years old'
-
+length=len(greeting)
+# print(greeting[length-1])
+# print(greeting[-1])
+# print(greeting[2:5])
+# print(greeting[3:])
+# print(greeting[:15])
+print(greeting[2:40:2])
 # print(greeting)
-print(greeting[0])
-print(len(greeting))
-print(greeting[])
-print(greeting[3])
+# print(greeting[0])
+# print(len(greeting))
+# print(greeting[])
+# print(greeting[3])
